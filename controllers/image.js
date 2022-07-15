2182ba5ad0b9cf725a1487e9ebc8c48d7898448e
Oki Kehinde <olamiposi.oki@gmail.com>
2022-07-15 14:15:42 +0100

    made route params work

--- controllers/image.js
+++ controllers/image.js
@@ -9,8 +9,17 @@ const addImages = async (req, res) => {
 	const images = await Image.create(req.body);
 	res.status(201).json({ images });
 };
+const getImage = async (req, res, next) => {
+	const { label: labelName } = req.params;
+	const image = await Image.findOne({ label: labelName });
+	if (!image) {
+		return next(res.send('No Image with that label'));
+	}
+	res.status(200).json({ image });
+};
 
 module.exports = {
 	getImages,
 	addImages,
+	getImage,
 };
--- routes/image.js
+++ routes/image.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const { getImages, addImages } = require('../controllers/image.js');
+const { getImages, addImages, getImage } = require('../controllers/image.js');
 
 router.route('/').get(getImages).post(addImages);
-
+router.route('/:label').get(getImage);
 module.exports = router;
