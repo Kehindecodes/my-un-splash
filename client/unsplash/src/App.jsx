e0dc7dad1a3f450a8456354c5d5e7aafd85e3b2d
Oki Kehinde <olamiposi.oki@gmail.com>
2022-07-19 20:20:37 +0100

    set up the client side evironment

---
+++ client/unsplash/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+     <h1>my unsplash</h1>
+    </div>
+  )
+}
+
+export default App
