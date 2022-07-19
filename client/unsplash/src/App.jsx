03a0baf9a583e5eae6452f978383647ab6dfa5a3
Oki Kehinde <olamiposi.oki@gmail.com>
2022-07-19 22:02:54 +0100

    finish Header components

--- client/unsplash/src/App.jsx
+++ client/unsplash/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <div className="App">
-     <h1>my unsplash</h1>
+     <Header/>
     </div>
   )
 }
--- client/unsplash/tailwind.config.cjs
+++ client/unsplash/tailwind.config.cjs
@@ -2,7 +2,20 @@
 module.exports = {
 	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		colors: {
+			grey: '#bdbdbd',
+			gren: '#3DB46D',
+			white: '#ffffff',
+		},
+		fontFamily: {
+			noto: ['Noto Sans', 'sans-serif'],
+			serif: ['Merriweather', 'serif'],
+		},
+		extend: {
+			width: {
+				c: '300px',
+			},
+		},
 	},
-	plugins: [],
+	plugins: [require('@tailwindcss/forms')],
 };
