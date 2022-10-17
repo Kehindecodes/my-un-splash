8904a32244a6b16bc37e035aa396ac497c3e3e3c
Oki Kehinde <olamiposi.oki@gmail.com>
2022-10-17 22:44:43 +0100

    add functionality done

--- client/unsplash/src/App.jsx
+++ client/unsplash/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import DisplayImages from './components/DisplayImages';
 import Header from './components/Header';
-import useSWR from 'swr';
+import useSWR, { useSWRConfig } from 'swr';
 import axios from 'axios';
 import AddModal from './components/AddModal';
 
@@ -11,9 +11,12 @@ const fetcher = async (url) => {
 	return res.data.images;
 };
 function App() {
+	const { mutate } = useSWRConfig();
 	const { data, error } = useSWR('/api/v1/images', fetcher);
 	const [searchTerm, setSearchTerm] = useState('');
 	const [modalIsOpen, setIsOpen] = useState(false);
+	const [label, setLabel] = useState('');
+	const [url, setUrl] = useState('');
 	// const [images, setImages] = useState(data);
 	// console.log(data);
 
@@ -21,11 +24,53 @@ function App() {
 		setSearchTerm(e.target.value);
 		console.log(e.target.value);
 	};
+	const changeLabel = (e) => {
+		setLabel(e.target.value);
+		console.log(e.target.value);
+	};
+	const changeUrl = (e) => {
+		setUrl(e.target.value);
+		console.log(e.target.value);
+	};
 	const openModal = () => {
 		setIsOpen(true);
 	};
 	const closeModal = () => {
 		setIsOpen(false);
+	};
+	const updateFn = async (image) => {
+		const images = [...data, image];
+		return images;
+	};
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const newImage = {
+			label: label,
+			url: url,
+		};
+		// Update the local state immediately and fire the
+		// request. Since the API will return the updated
+		// data, there is no need to start a new revalidation
+		// and we can directly populate the cache.
+		await mutate('/api/v1/images', updateFn(newImage), {
+			optimisticData: [...data, newImage],
+			rollbackOnError: true,
+			populateCache: true,
+			revalidate: false,
+		});
+		// send text to the Api
+		await fetch('/api/v1/images', {
+			method: 'POST',
+			body: JSON.stringify(newImage),
+			headers: {
+				'Content-type': 'application/json; charset=UTF-8',
+			},
+		});
+
+		// setLabel('');
+		// setUrl('');
+		closeModal();
+		console.log('sent..');
 	};
 
 	return (
@@ -36,6 +81,11 @@ function App() {
 				openModal={openModal}
 				closeModal={closeModal}
 				modalIsOpen={modalIsOpen}
+				changeLabel={changeLabel}
+				changeUrl={changeUrl}
+				handleSubmit={handleSubmit}
+				label={label}
+				url={url}
 			/>
 		</div>
 	);
--- client/unsplash/src/components/AddModal.jsx
+++ client/unsplash/src/components/AddModal.jsx
@@ -1,7 +1,16 @@
 import { useState, useRef, Fragment } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 
-const AddModal = ({ openModal, closeModal, modalIsOpen }) => {
+const AddModal = ({
+	openModal,
+	closeModal,
+	modalIsOpen,
+	changeLabel,
+	changeUrl,
+	handleSubmit,
+	label,
+	url,
+}) => {
 	const cancelButtonRef = useRef(null);
 	return (
 		<Transition.Root show={modalIsOpen} as={Fragment}>
@@ -53,7 +62,7 @@ const AddModal = ({ openModal, closeModal, modalIsOpen }) => {
 											className='text-lg leading-6 text-text font-noto font-medium mb-4'>
 											Add a new photo
 										</Dialog.Title>
-										<form className='mt-2 w-full'>
+										<form className='mt-2 w-full' onSubmit={handleSubmit}>
 											<div>
 												<label className='block'>
 													<span className='font-medium text-sm text-text2'>
@@ -63,6 +72,8 @@ const AddModal = ({ openModal, closeModal, modalIsOpen }) => {
 														className='mt-1 block w-full rounded-xl placeholder:text-placeholder '
 														type='text'
 														placeholder='Suspendisse elit massa'
+														value={label}
+														onChange={changeLabel}
 													/>
 												</label>
 
@@ -74,34 +85,35 @@ const AddModal = ({ openModal, closeModal, modalIsOpen }) => {
 														className='mt-1 block w-full rounded-xl placeholder:text-placeholder'
 														type='text'
 														placeholder='https://images.unsplash.com/photo-1584395630827-860eee694d7b?ixlib=r...'
+														value={url}
+														onChange={changeUrl}
 													/>
 												</label>
 											</div>
-										</form>
-									</div>
-								</div>
-							</div>
-							<div className='bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse mt-5'>
-								<button
-									type='button'
-									className='w-full inline-flex justify-center
+											<div className='bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse mt-5'>
+												<button
+													type='submit'
+													className='w-full inline-flex justify-center
                      border border-transparent shadow-sm px-4 py-2 text-white bg-gren  font-noto rounded-xl p-3 text-sm  hover:bg-red-700 
                       focus:outline-none
- sm:ml-3 sm:w-auto sm:text-sm'
-									onClick={closeModal}>
-									Submit
-								</button>
-								<button
-									type='button'
-									className='mt-3 w-full inline-flex justify-center
+ sm:ml-3 sm:w-auto sm:text-sm'>
+													Submit
+												</button>
+												<button
+													type='button'
+													className='mt-3 w-full inline-flex justify-center
                     rounded-md border border-none shadow-sm px-4 py-2
                      bg-white text-placeholder font-medium text-gray-700
                       hover: border-red  sm:mt-0
                         sm:ml-3 sm:w-auto sm:text-sm'
-									onClick={closeModal}
-									ref={cancelButtonRef}>
-									Cancel
-								</button>
+													onClick={closeModal}
+													ref={cancelButtonRef}>
+													Cancel
+												</button>
+											</div>
+										</form>
+									</div>
+								</div>
 							</div>
 						</div>
 					</Transition.Child>
