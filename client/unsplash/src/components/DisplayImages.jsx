4ddff9f7b85c6347ba9bf2e0512c0efe2fd1f4e1
Oki Kehinde <olamiposi.oki@gmail.com>
2023-03-28 16:46:29 +0100

    add a NotFound component to show images not found

--- client/unsplash/src/components/DisplayImages.jsx
+++ client/unsplash/src/components/DisplayImages.jsx
@@ -1,5 +1,6 @@
 import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
+import NotFound from './NotFound';
 
 const DisplayImages = ({ images, error, searchItem, handleDelete }) => {
 	// const returnErr = () => console.log(data.images);
@@ -8,18 +9,26 @@ const DisplayImages = ({ images, error, searchItem, handleDelete }) => {
 	if (!images) return <div>Loading....</div>;
 	const reversed = [...images].reverse();
 	console.log(reversed);
+
+	// check if any images match the search item
+	const filteredImages = reversed.filter((filteredImage) => {
+		if (searchItem === '') {
+			return filteredImage;
+		} else if (
+			filteredImage.label.toLowerCase().includes(searchItem.toLowerCase())
+		) {
+			return filteredImage;
+		} else {
+			return null;
+		}
+	});
+	const imagesFound = filteredImages.length > 0;
+
 	return (
-		<div className=' gap-8 xs:gap-0 md:gap-8   xs:columns-1 sm:columns-2 md:columns-3 my-12  xs:w-full  '>
-			{reversed
-				.filter((filteredImage) => {
-					if (searchItem === '') {
-						return filteredImage;
-					} else if (
-						filteredImage.label.toLowerCase().includes(searchItem.toLowerCase())
-					)
-						return filteredImage;
-				})
-				.map((image, key) => (
+		<div className=' gap-8 xs:gap-0 md:gap-8   xs:columns-1 sm:columns-2 md:columns-3 my-12  xs:w-full flex-1  '>
+			{!imagesFound && <NotFound />}
+			{imagesFound &&
+				filteredImages.map((image, key) => (
 					<div
 						key={key}
 						className='relative group wrapper hover:cursor-pointer mb-4 w-full img-wrapper'>
---
+++ client/unsplash/src/components/NotFound.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const NotFound = () => {
+	return (
+		<div className='flex items-center justify-center h-screen w-full'>
+			<div className='text-center'>
+				<h1
+					className='text-6x1 font-bold
+			'>
+					No images found
+				</h1>
+			</div>
+		</div>
+	);
+};
+
+export default NotFound;
